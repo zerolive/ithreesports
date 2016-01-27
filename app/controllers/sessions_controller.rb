7583ef6f71e6aa32a39aef771232cebc9f69ed31
ZeroLive <zerolive@gmail.com>
2016-01-27 11:48:29 +0100

    Created sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		@user = User.find_by(name: params[:name])
+		if user_authenticate
+			session[:user_id] = @user.id
+			redirect_to admin_path
+		else
+			redirect_to root_path
+		end
+	end
+
+	def destroy
+   		session.delete(:user_id)
+   		redirect_to root_path
+	end
+
+	private
+
+	def user_authenticate
+		return false unless @user
+		return false unless @user.password == params[:password]
+		true
+	end
+end
