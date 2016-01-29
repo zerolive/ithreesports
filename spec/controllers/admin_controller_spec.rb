f7d14854d74af26f6cdd42fbb25e1fa511747391
ZeroLive <zerolive@gmail.com>
2016-01-29 12:11:20 +0100

    Added test for update user

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe AdminController, type: :controller do
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
 			expect(number_of_users + 1).to eq User.count
+			expect(assigns(:user).errors.size).to eq 0
 		end
 
 		it 'cannot create a user without level' do
@@ -35,6 +36,7 @@ RSpec.describe AdminController, type: :controller do
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
 			expect(number_of_users).to eq User.count
+			expect(assigns(:user).errors.size).to_not eq 0
 		end
 
 		it 'cannot create a user without password' do
@@ -44,6 +46,7 @@ RSpec.describe AdminController, type: :controller do
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
 			expect(number_of_users).to eq User.count
+			expect(assigns(:user).errors.size).to_not eq 0
 		end
 
 		it 'cannot create a user without name' do
@@ -53,6 +56,7 @@ RSpec.describe AdminController, type: :controller do
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
 			expect(number_of_users).to eq User.count
+			expect(assigns(:user).errors.size).to_not eq 0
 		end
 	end
 
@@ -64,5 +68,45 @@ RSpec.describe AdminController, type: :controller do
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
 		end
+	end
+
+	describe 'patch#update_user' do
+		it ' saves the changes and redirects to users index' do
+			user_to_edit = create(:user, name: 'OldName')
+			patch :update_user, id: user_to_edit.id, user: { name: 'NewName', password: user_to_edit.password, level: user_to_edit.level}
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_users_path
+			expect(assigns(:user).name).to eq 'NewName'
+			expect(assigns(:user).errors.size).to eq 0
+		end
+
+		it ' cannot save a user without name and redirects to users index' do
+			user_to_edit = create(:user)
+			patch :update_user, id: user_to_edit.id, user: { name: nil, password: user_to_edit.password, level: user_to_edit.level}
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_users_path
+			expect(assigns(:user).errors.size).to_not eq 0
+		end
+
+		it ' cannot save a user without password and redirects to users index' do
+			user_to_edit = create(:user)
+			patch :update_user, id: user_to_edit.id, user: { name: user_to_edit.name, password: nil, level: user_to_edit.level}
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_users_path
+			expect(assigns(:user).errors.size).to_not eq 0
+		end
+
+		it ' cannot save a user without level and redirects to users index' do
+			user_to_edit = create(:user)
+			patch :update_user, id: user_to_edit.id, user: { name: user_to_edit.name, password: user_to_edit.password, level: nil }
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_users_path
+			expect(assigns(:user).errors.size).to_not eq 0
+		end
+
 	end
 end
