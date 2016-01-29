077b9ff617ad72a0859dc9e7f3793d957888ec2b
ZeroLive <zerolive@gmail.com>
2016-01-29 12:10:32 +0100

    Added route for update user

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   get '/admin' => 'admin#index'
   get '/admin_users' => 'admin#users_index'
   get '/admin_courses' => 'admin#courses_index'
+
   post '/create_user' => 'admin#create_user'
   delete '/delete_user/:id' => 'admin#delete_user', as: :delete_user
   get '/edit_user/:id' => 'admin#edit_user', as: :edit_user
+  patch '/update_user/:id' => 'admin#update_user', as: :update_user
 end
