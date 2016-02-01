d3db5ab0a0fe27f578335c8b98a249fdea8a543d
ZeroLive <zerolive@gmail.com>
2016-02-01 16:04:32 +0100

    Added routes for new, index and create exam

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
 
   get '/admin' => 'admin#index'
   get '/admin_users' => 'admin#users_index'
-  get '/admin_courses' => 'admin#courses_index'
 
   post '/create_user' => 'admin#create_user'
   delete '/delete_user/:id' => 'admin#delete_user', as: :delete_user
   get '/edit_user/:id' => 'admin#edit_user', as: :edit_user
   patch '/update_user/:id' => 'admin#update_user', as: :update_user
+
+  get '/admin_exams' => 'admin#admin_exams'
+  post '/create_exam' => 'admin#create_exam'
 end
