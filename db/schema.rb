620bacd99aad792d67e93dfbb285639ad56377b3
ZeroLive <zerolive@gmail.com>
2016-02-01 14:42:22 +0100

    Migration created for add user_id column to question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125131833) do
+ActiveRecord::Schema.define(version: 20160201133014) do
+
+  create_table "exams", force: :cascade do |t|
+    t.string   "title"
+    t.string   "level"
+    t.string   "video"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "exam_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
