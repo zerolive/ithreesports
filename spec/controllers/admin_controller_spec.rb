fb7f129dd25dba2fece5c03eb0203013bd96c690
ZeroLive <zerolive@gmail.com>
2016-02-01 18:27:26 +0100

    Added test for edit, update and delete exams in admin controller

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -20,42 +20,34 @@ RSpec.describe AdminController, type: :controller do
 	describe 'post#create_user' do
 		let(:new_user){ build(:user) }
 		it 'can create a user and redirect to admin user index' do
-			number_of_users = User.count
 			post :create_user, user: {name: new_user.name, password: new_user.password, level: new_user.level}
 
 			expect(response.status).to eq 302
-			expect(response).to redirect_to admin_users_path
-			expect(number_of_users + 1).to eq User.count
 			expect(assigns(:user).errors.size).to eq 0
+			expect(response).to redirect_to admin_users_path
 		end
 
 		it 'cannot create a user without level' do
-			number_of_users = User.count
 			post :create_user, user: {name: new_user.name, password: new_user.password, level: nil}
 
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
-			expect(number_of_users).to eq User.count
 			expect(assigns(:user).errors.size).to_not eq 0
 		end
 
 		it 'cannot create a user without password' do
-			number_of_users = User.count
 			post :create_user, user: {name: new_user.name, password: nil, level: new_user.level}
 
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
-			expect(number_of_users).to eq User.count
 			expect(assigns(:user).errors.size).to_not eq 0
 		end
 
 		it 'cannot create a user without name' do
-			number_of_users = User.count
 			post :create_user, user: {name: nil, password: new_user.password, level: new_user.level}
 
 			expect(response.status).to eq 302
 			expect(response).to redirect_to admin_users_path
-			expect(number_of_users).to eq User.count
 			expect(assigns(:user).errors.size).to_not eq 0
 		end
 	end
@@ -107,6 +99,114 @@ RSpec.describe AdminController, type: :controller do
 			expect(response).to redirect_to admin_users_path
 			expect(assigns(:user).errors.size).to_not eq 0
 		end
+	end
 
+	describe 'get#admin_exams' do
+		it 'response with status OK' do
+			get :admin_exams
+
+			expect(response.status).to eq 200
+		end
+	end
+
+	describe 'post#create_exam' do
+		let(:new_exam){ build(:exam) }
+
+		it 'can create an exam and redirects to admin course' do
+			post :create_exam, exam: {title: new_exam.title, video: new_exam.video, level: new_exam.level, comment: new_exam.comment }
+
+			expect(assigns(:exam).errors.size).to eq 0
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
+
+		it 'can create an exam without video and comment and redirects to admin course' do
+			post :create_exam, exam: {title: new_exam.title, video: nil, level: new_exam.level, comment: nil }
+
+			expect(assigns(:exam).errors.size).to eq 0
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
+
+		it 'can create an exam without video and redirects to admin course' do
+			post :create_exam, exam: {title: new_exam.title, video: nil, level: new_exam.level, comment: new_exam.comment }
+
+			expect(assigns(:exam).errors.size).to eq 0
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
+
+		it 'cannot create a exam without title and redirects to admin course' do
+			post :create_exam, exam: {title: nil, video: new_exam.video, level: new_exam.level, comment: new_exam.comment }
+
+			expect(assigns(:exam).errors.size).to_not eq 0
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
+
+		it 'cannot create a exam without level and redirects to admin course' do
+			post :create_exam, exam: {title: new_exam.title, video: new_exam.video, level: nil, comment: new_exam.comment }
+
+			expect(assigns(:exam).errors.size).to_not eq 0
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
+	end
+
+	describe 'get#edit_exam' do
+		let(:exam_to_edit){ create(:exam) }
+
+		it 'response with status OK' do
+			get :edit_exam, id: exam_to_edit.id
+
+			expect(response.status).to eq 200
+		end
+	end
+
+	describe 'patch#update_exam' do
+		let(:exam_to_edit){ create(:exam) }
+
+		it 'update an exam and redirects to admin exams path' do
+			patch :update_exam, id: exam_to_edit.id, exam: { title: 'NewTitle', video: exam_to_edit.video, level: exam_to_edit.level, comment: exam_to_edit.comment }
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+			expect(assigns(:exam).errors.size).to eq 0
+		end
+
+		it 'update an exam without video and comment and redirects to admin exams path' do
+			patch :update_exam, id: exam_to_edit.id, exam: { title: exam_to_edit.title, video: nil, level: exam_to_edit.level, comment: nil }
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+			expect(assigns(:exam).errors.size).to eq 0
+		end
+
+		it 'cannot update an exam without title' do
+			patch :update_exam, id: exam_to_edit.id, exam: { title: nil, video: exam_to_edit.video, level: exam_to_edit.level, comment: exam_to_edit.comment  }
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+			expect(assigns(:exam).errors.size).to_not eq 0
+		end
+
+		it 'cannot update an exam without level' do
+			patch :update_exam, id: exam_to_edit.id, exam: { title: exam_to_edit.title, video: exam_to_edit.video, level: nil, comment: exam_to_edit.comment  }
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+			expect(assigns(:exam).errors.size).to_not eq 0
+		end
+	end
+
+	describe 'delete#delete_exam' do
+		let(:exam_to_delete){ create(:exam) }
+
+		it 'deletes an exam and redirects to admin exams' do
+			delete :delete_exam, id: exam_to_delete.id
+
+			expect(response.status).to eq 302
+			expect(response).to redirect_to admin_exams_path
+		end
 	end
 end
