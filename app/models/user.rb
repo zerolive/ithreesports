4222e22dd5e9b93d411c5ab2efe33ff04f5617be
ZeroLive <zerolive@gmail.com>
2016-02-01 14:43:10 +0100

    Tabuled correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	LEVELS = [ '1', '2', '3', 'Admin']
+	
 	validates :name, presence: true, uniqueness: true, length: { in: 5..12 }
 	validates :password, presence: true, length: { in: 8..12 }
 	validates :level, presence: true, inclusion: LEVELS
