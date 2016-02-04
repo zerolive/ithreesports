979dbf196ef76fa2ce6f20960dbd3daf271819ce
ZeroLive <zerolive@gmail.com>
2016-02-04 16:48:43 +0100

    Created a new factory for admin user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,17 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :user do
+  factory :user, class: User do
     name Faker::Lorem.characters(8)
     password Faker::Internet.password(8)
     level '1'
+  end
+end
+
+FactoryGirl.define do
+  factory :admin, class: User do
+    name Faker::Lorem.characters(8)
+    password Faker::Internet.password(8)
+    level 'Admin'
   end
 end
