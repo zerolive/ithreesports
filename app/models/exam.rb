3e63d504cd0546524c1e71289f3fe8d63bbf8f6a
ZeroLive <zerolive@gmail.com>
2016-02-04 15:25:31 +0100

    Added method video_id to model exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,4 +1,6 @@
 class Exam < ActiveRecord::Base
+	YOUTUBE_SORT = 'youtu.be'
+	YOUTUBE_LONG = 'youtube.com'
 	LEVELS = [ '1', '2', '3']
 
 	validates :title, presence: true
@@ -8,5 +10,12 @@ class Exam < ActiveRecord::Base
 
 	def self.levels
 		LEVELS
+	end
+
+	def video_id
+		result = ''
+		result = video.match(/(?:^.*\/)([^#?]*)/)[1] if video.include? YOUTUBE_SORT
+		result = video.match(/(?:^.*\=)(.*)/)[1] if video.include? YOUTUBE_LONG
+		result
 	end
 end
