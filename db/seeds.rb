c657eb4da6345bc10cdc614ad089d75b07e1888f
ZeroLive <zerolive@gmail.com>
2016-02-09 19:26:59 +0100

    Changed seeds and added exams and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "admin", password: "12345678", level: "Admin")
+User.create!(name: "user1", password: "12345678", level: "1")
+User.create!(name: "user2", password: "12345678", level: "2")
+User.create!(name: "user3", password: "12345678", level: "3")
 
-User.create!(name: 'admin', password: '12345678', level: 'Admin')
-User.create!(name: 'user1', password: '12345678', level: '1')
-User.create!(name: 'user2', password: '12345678', level: '2')
-User.create!(name: 'user3', password: '12345678', level: '3')
+(1..3).each do |t|
+	first = Exam.create!(title: "First exam for level #{t}", level: t.to_s, video: "https://www.youtube.com/watch?v=g6p7eTejWRY", comment: "Comment for first exam level #{t}")
+	second = Exam.create!(title: "Second exam for level #{t}", level: t.to_s, video: "https://www.youtube.com/watch?v=g6p7eTejWRY", comment: "Comment for second exam level #{t}")
+	Question.create!(title: "First question for first exam level #{t}", answer: true, exam_id: first.id)
+	Question.create!(title: "Second question for first exam level #{t}", answer: false, exam_id: first.id)
+	Question.create!(title: "First question for second exam level #{t}", answer: true, exam_id: second.id)
+	Question.create!(title: "Second question for second exam level #{t}", answer: false, exam_id: second.id)
+end
