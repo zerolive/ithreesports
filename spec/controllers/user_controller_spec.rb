96fd1e726af727357102d4d1a0d664c8bd3d200e
ZeroLive <zerolive@gmail.com>
2016-02-09 19:27:30 +0100

    Added test for user index controller

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
 
+	describe 'get#index' do
+		let(:user){ create(:user)}
+
+		it 'respond with status OK' do
+			session[:user_id] = user.id
+
+			get :index
+
+			expect(response.status).to eq 200
+		end
+	end
 end
