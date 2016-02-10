ddd1ec7e11949a33fbf454fd1f84fb8da3675135
ZeroLive <zerolive@gmail.com>
2016-02-10 19:04:34 +0100

    Selected exams for user by user level and lower

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,16 @@ class UserController < ApplicationController
 
 	def index
 		@user = User.find(session[:user_id])
-		@exams = Exam.where(level: @user.level)
+		level = @user.level.to_i
+		@exams = []
+		Exam.all.each do |exam|
+			@exams << exam if exam.level.to_i <= level
+		end
+	end
+
+	def user_exam
+		@exam = Exam.find(params[:id])
+		@questions = @exam.questions
+		@completed_exam = CompletedExam.new
 	end
 end
