8747801af6b8a8f49b58ddcacd3b9aad2d4deb63
ZeroLive <zerolive@gmail.com>
2016-02-11 19:05:16 +0100

    Added method user exam save to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,11 +7,53 @@ class UserController < ApplicationController
 		Exam.all.each do |exam|
 			@exams << exam if exam.level.to_i <= level
 		end
+		@completed_exam = CompletedExam.where(user_id: @user.id)
 	end
 
 	def user_exam
 		@exam = Exam.find(params[:id])
 		@questions = @exam.questions
-		@completed_exam = CompletedExam.new
 	end
+
+	def user_exam_save
+		exam = Exam.find(params[:id])
+
+		delete_old_completed_exam exam.id
+
+		score = calculate_score params
+		new_completed_exam exam, score
+
+		redirect_to user_path
+	end
+
+	private
+
+	def calculate_score params
+		score = 0
+		params.each do | key, value |
+			if key.to_i != 0
+				question = Question.find(key.to_i)
+				answer = false
+				answer = true if value == '1'
+				score += 1 if answer == question.answer
+			end
+		end
+		score
+	end
+
+	def delete_old_completed_exam exam_id
+		old_completed_exam = CompletedExam.where(user_id: session[:user_id]).where(exam_id: exam_id)
+		old_completed_exam.destroy if old_completed_exam.nil?
+	end
+
+	def new_completed_exam exam, score
+		completed_exam = CompletedExam.new({
+				:user_id => session[:user_id], 
+				:answers => exam.questions.count, 
+				:score => score, 
+				:exam_id => exam.id 
+			})
+		completed_exam.save
+	end
+
 end
