cf6c9d94b4ad849c1d104be660738359359b9080
ZeroLive <zerolive@gmail.com>
2016-02-11 19:06:59 +0100

    Changed answers from hast to integer and added exam id field

--- app/models/completed_exam.rb
+++ app/models/completed_exam.rb
@@ -1,9 +1,9 @@
 class CompletedExam < ActiveRecord::Base
-	serialize :answers,Hash
 
 	validates :user_id, presence: true
 	validates :answers, presence: true
 	validates :score, presence: true
+	validates :exam_id, presence: true
 
 	belongs_to :user
 end
