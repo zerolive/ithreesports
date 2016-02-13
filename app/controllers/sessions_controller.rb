cd597e3a5ef0b7ebbcd20c8f9158d4a973e409b6
ZeroLive <zerolive@gmail.com>
2016-02-13 04:54:41 +0100

    Added downcase to name param

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(name: params[:name])
+		@user = User.find_by(name: params[:name].downcase)
 		if user_authenticate
 			session[:user_id] = @user.id
 			redirect_to admin_path if @user.level == 'Admin'
