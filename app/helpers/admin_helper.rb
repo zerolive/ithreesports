e0c0a9d6f766c3fbf44f8a002bc0365d0f3911b0
ZeroLive <zerolive@gmail.com>
2016-02-21 05:03:17 +0100

    Moved helpers methods from admin helper to application helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,12 +1,3 @@
 module AdminHelper
-	def checked boolean
-		result = "<input type='checkbox'>"
-		result = "<input type='checkbox' checked>" if boolean
-		result.html_safe
-	end
 
-	def youtube_iframe video_id
-		"<iframe id='ytplayer' type='text/html' width='640' height='390'
-		  src='https://www.youtube.com/embed/#{video_id}?autoplay'=1='0'/></iframe>".html_safe
-	end
 end
