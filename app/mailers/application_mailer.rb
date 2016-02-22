a5d6aa439947a2b012eb454d0662fcc4aa2a81c8
ZeroLive <zerolive@gmail.com>
2016-02-22 14:04:28 +0100

    configured email name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@i3spots.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-	default from: 'notifications@example.com'
+	default from: 'noreply@i3sports.com'
  
 	def welcome_email(user)
 		@user = user
-		@url  = 'http://example.com/login'
-		mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+		@url  = 'http://www.i3sports.com/login'
+		mail(to: @user.email, subject: t(:welcome_email_subject))
 	end
 end
