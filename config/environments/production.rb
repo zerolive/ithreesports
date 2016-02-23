a96841f672cdd281682d00f9487ddded01556550
ZeroLive <zerolive@gmail.com>
2016-02-23 18:40:33 +0100

    Added mailer config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SENDGRID_SERVER'],
+    port:                 '587',
+    domain:               'example.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
