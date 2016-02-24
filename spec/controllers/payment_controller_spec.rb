76a0d19b6c9571c7d123ba470dc8c74e0d1d1214
ZeroLive <zerolive@gmail.com>
2016-02-24 15:30:41 +0100

    Added test for thanks purchase

--- spec/controllers/payment_controller_spec.rb
+++ spec/controllers/payment_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PaymentController, type: :controller do
+	describe 'get#thanks' do
+		it 'responds with status ok' do
+			get :thanks
 
+			expect(response.status).to eq 200
+		end
+	end
 end
