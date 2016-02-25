bd99ada7c4568957b43abea59498316ac82ed4c4
ZeroLive <zerolive@gmail.com>
2016-02-25 13:18:02 +0100

    Added gender field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223111023) do
+ActiveRecord::Schema.define(version: 20160224151450) do
 
   create_table "completed_exams", force: :cascade do |t|
     t.integer  "user_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160223111023) do
     t.datetime "updated_at",      null: false
     t.string   "email"
     t.string   "password_digest"
+    t.string   "gender"
   end
 
 end
