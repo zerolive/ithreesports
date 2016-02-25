dafcd4d4dbe3eec4c4e47b12d1397783be83e2da
ZeroLive <zerolive@gmail.com>
2016-02-25 13:20:02 +0100

    Added method avatar_for(gender) in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,13 @@ module ApplicationHelper
 		"<a href='https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_xclick&business=antonioc3brian%2dfacilitator%40gmail%2ecom&lc=US&item_name=#{item_name}&item_number=1&amount=#{quantity}%2e00&currency_code=USD&notify_url=https%3a%2f%2fgentle%2dwaters%2d42879%2eherokuapp%2ecom%2fpayed%2f&return=https%3a%2f%2fgentle%2dwaters%2d42879%2eherokuapp%2ecom%2fpayed%2f&button_subtype=services&no_note=0&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHostedGuest'>#{link_name}</a>".html_safe
 	end
 
+	def avatar_for(gender)
+		return 'mujer' if gender == 'Mujer'
+		'hombre'
+	end
+
+	private
+
 	def item_text(quantity)
 		return "Curso%20Basico" if quantity == 10
 		return "Curso%20Avanzado" if quantity == 15
