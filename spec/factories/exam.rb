58c7d8f4441a280207aa21d9d18dcc17fad3a4ad
ZeroLive <zerolive@gmail.com>
2016-02-27 04:26:35 +0100

    Fixed exam factory

--- spec/factories/exam.rb
+++ spec/factories/exam.rb
@@ -2,18 +2,18 @@ require 'faker'
 
 FactoryGirl.define do
   factory :exam do
-  	title Faker::Name.title
-      position Faker::Number.number(1)
-  	video Faker::Internet.url
-  	comment Faker::Lorem.characters(200)
-   level '1'
-  	factory :exam_with_questions do
-	   	transient do
-	      	questions_count 10
-	   end
-	   	after(:create) do |exam, evaluator|
-	   		create_list(:question, evaluator.questions_count, exam: exam)
-	   	end
-  end
+    title Faker::Name.title
+    position Faker::Number.number(1)
+    video Faker::Internet.url
+    comment Faker::Lorem.characters(200)
+    level '1'
+    factory :exam_with_questions do
+      transient do
+        questions_count 10
+      end
+      after(:create) do |exam, evaluator|
+        create_list(:question, evaluator.questions_count, exam: exam)
+      end
+    end
   end
 end
