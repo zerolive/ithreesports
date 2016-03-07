e61945ada56087553909f46159a3d520478ed975
ZeroLive <zerolive@gmail.com>
2016-03-07 17:55:44 +0100

    Added media_files to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,13 @@ User.create!(name: "user3", email: "user3@test.com", password: "12345678", passw
 	Answer.create!(question_id: second_question_second_exam.id, title: 'Answer 3', right: '1')
 	Answer.create!(question_id: second_question_second_exam.id, title: 'Answer 4', right: '0')
 end
+
+first_course = Course.create!(name: "First course", imageurl: "http://thumbs1.ebaystatic.com/d/l225/pict/281952688656_-1.jpg", price: 10, description: "Description for first course", videourl: "https://www.youtube.com/watch?v=iL0m5XRkdtg")
+								MediaFile.create!(course_id: first_course.id, url: "http://thumbs1.ebaystatic.com/d/l225/pict/281952688656_-1.jpg", video: "0")
+								MediaFile.create!(course_id: first_course.id, url: "https://www.youtube.com/watch?v=iL0m5XRkdtg", video: "1")
+second_course = Course.create!(name: "Second course", imageurl: "http://www.heyboitila.com/wp-content/uploads/2014/09/SECOND-COURSE-ARCHIVO-ESPANOL1.jpg", price: 20, description: "Description for second course", videourl: "https://www.youtube.com/watch?v=TZhfJ0maZ2E")
+								MediaFile.create!(course_id: second_course.id, url: "http://www.heyboitila.com/wp-content/uploads/2014/09/SECOND-COURSE-ARCHIVO-ESPANOL1.jpg", video: "0")
+								MediaFile.create!(course_id: second_course.id, url: "https://www.youtube.com/watch?v=TZhfJ0maZ2E", video: "1")
+third_course = Course.create!(name: "Third course", imageurl: "http://cdn.rainbowresource.netdna-cdn.com/products/047825.jpg", price: 30, description: "Description for thrid course", videourl: "https://www.youtube.com/watch?v=k6BQ-uTo_TI")
+								MediaFile.create!(course_id: third_course.id, url: "http://cdn.rainbowresource.netdna-cdn.com/products/047825.jpg", video: "0")
+								MediaFile.create!(course_id: third_course.id, url: "https://www.youtube.com/watch?v=k6BQ-uTo_TI", video: "1")
