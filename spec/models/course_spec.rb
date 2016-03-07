c617a3e0608de435285af3d729a33db37b8af4a5
ZeroLive <zerolive@gmail.com>
2016-03-07 17:20:47 +0100

    Added test for model courses

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+	describe "Validations" do
+		let(:new_course){ build(:course) }
+		it "can create a course" do
+			expect(new_course.save).to eq true
+		end
+
+		it "cannot create a course without name" do
+			new_course.name = nil
+
+			expect(new_course.save).to_not eq true
+		end
+		it "cannot create a course without price" do
+			new_course.price = nil
+
+			expect(new_course.save).to_not eq true
+		end
+		it "cannot create two courses with same price" do
+			old_course = create(:course)
+			new_course.price = old_course.price
+
+			expect(new_course.save).to_not eq true
+		end
+	end
+	describe "Associations" do
+		it 'has many exams' do
+			expect(Course.reflect_on_association(:exam).macro).to eq(:has_many)
+		end
+	end
+end
