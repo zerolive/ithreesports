2468e68905a13d5c77db6c8592edd6855cb1ada3
ZeroLive <zerolive@gmail.com>
2016-03-23 15:39:17 +0100

    Test on development

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,26 +2,32 @@ class PaymentController < ApplicationController
 	protect_from_forgery except: :payed
 
 	def payed
-		if has_params
-			@user = User.find_by(email: params["payer_email"])
-			@course = Course.find_by(price: params["payment_gross"].to_i)
-			if @user.nil?
-				@user.email = params["payer_email"]
-				@user.name = params["first_name"] + " " + params["last_name"]
-				@user.password_digest = new_password
-				@user.level = User.LEVELS[0]
-			end
+		#if has_params
+			#@user = User.find_by(email: params["payer_email"])
+			#@course = Course.find_by(price: params["payment_gross"].to_i)
+			#if @user.nil?
+				#@user.email = params["payer_email"]
+				#@user.name = params["first_name"] + " " + params["last_name"]
+				#@user.password_digest = new_password
+				#@user.level = "User"
+			#end
 
-			if params["payment_status"] == 'Completed'
-				if @user.save
-					UserMailer.welcome_email(@user).deliver_later
-					@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
-					@purchased.save
-				end
-			end
-			redirect_to thanks_path
-		end
-		redirect_to signin_path
+			#if params["payment_status"] == "Completed"
+				#if @user.save
+					#UserMailer.welcome_email(@user).deliver_later
+					#@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
+					#@purchased.save
+				#end
+			#end
+			#redirect_to thanks_path
+		#end
+		#redirect_to signin_path
+		@user.email = params["payer_email"]
+		@user.name = params["first_name"] + " " + params["last_name"]
+		@user.password_digest = new_password
+		@user.level = "User"
+		@user.save
+		redirect_to thanks_path
 	end
 
 	def thanks
