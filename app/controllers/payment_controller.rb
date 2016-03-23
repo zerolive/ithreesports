ea0a66a885d9add9ce4957b02c7b9c22926f1b18
ZeroLive <zerolive@gmail.com>
2016-03-23 13:21:03 +0100

    Added check params before create a user

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,29 +2,36 @@ class PaymentController < ApplicationController
 	protect_from_forgery except: :payed
 
 	def payed
-		@user = User.find_by(email: params["payer_email"])
-		@course = Course.find_by(price: params["payment_gross"].to_i)
-		if @user.nil?
-			@user.email = params["payer_email"]
-			@user.name = params["first_name"] + " " + params["last_name"]
-			@user.password_digest = new_password
-			@user.level = User.LEVELS[0]
-		end
-
-		if params["payment_status"] == 'Completed'
-			if @user.save
-				UserMailer.welcome_email(@user).deliver_later
-				@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
-				@purchased.save
+		if has_params
+			@user = User.find_by(email: params[:payer_email])
+			@course = Course.find_by(price: params[:payment_gross].to_i)
+			if @user.nil?
+				@user.email = params[:payer_email]
+				@user.name = params[:first_name] + " " + params[:last_name]
+				@user.password_digest = new_password
+				@user.level = User.LEVELS[0]
 			end
+
+			if params[:payment_status] == 'Completed'
+				if @user.save
+					UserMailer.welcome_email(@user).deliver_later
+					@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
+					@purchased.save
+				end
+			end
+			redirect_to thanks_path
 		end
-		redirect_to thanks_path
+		redirect_to signin_path
 	end
 
 	def thanks
 	end
 
 	private
+
+	def has_params
+		params[:payer_email] && params[:payment_gross] && params[:payer_email] && params[:first_name] && params[:last_name]
+	end
 
 	def level quantity
 		return '1' if quantity == '10.00'
