951fb80edc9d3bc4d09c187d96a3f9c023849b50
ZeroLive <zerolive@gmail.com>
2016-03-23 16:54:59 +0100

    JESUS HELP MEgit add .git add .git add .!

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,31 +2,27 @@ class PaymentController < ApplicationController
 	protect_from_forgery except: :payed
 
 	def payed
-		#if has_params
-			#@user = User.find_by(email: params["payer_email"])
-			#@course = Course.find_by(price: params["payment_gross"].to_i)
-			#if @user.nil?
-				#@user.email = params["payer_email"]
-				#@user.name = params["first_name"] + " " + params["last_name"]
-				#@user.password_digest = new_password
-				#@user.level = "User"
-			#end
-
-			#if params["payment_status"] == "Completed"
-				#if @user.save
-					#UserMailer.welcome_email(@user).deliver_later
-					#@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
-					#@purchased.save
-				#end
-			#end
-			#redirect_to thanks_path
-		#end
-		#redirect_to signin_path
 		params.permit!
-		@user = User.new(email: "new@test.com", name: "newnewnew", password_digest: "12345678", level: "User", gender: "Hombre")
-		@user.save if params
-		puts "*"*100, @user
-		redirect_to thanks_path
+		if has_params
+			@user = User.find_by(email: params[:payer_email])
+			@course = Course.find_by(price: params[:payment_gross].to_i)
+			if @user.nil?
+				@user.email = params[:payer_email]
+				@user.name = params[:first_name] + " " + params[:last_name]
+				@user.password_digest = new_password
+				@user.level = "User"
+			end
+
+			if params[:payment_status] == "Completed"
+				if @user.save
+					UserMailer.welcome_email(@user).deliver_later
+					@purchased = Purchased.new(user_id: @user.id, course_id: @course.id)
+					@purchased.save
+				end
+			end
+			redirect_to thanks_path
+		end
+		redirect_to signin_path
 	end
 
 	def thanks
